61c0f62568da9c5e6448320b746465ec55715968
Gabriel Almeida <ggalmeida1@users.noreply.github.com>
2021-07-19 13:55:09 -0300

    Bugs: No CSS on Edit Series page && Can't render /series page

---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+const mongo = process.env.MONGODB || 'mongodb://localhost/minhas-series'
+const pages = require('./routes/pages')
+const series = require('./routes/series')
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+//process request body
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//assets
+app.use(express.static('public'))
+
+//view engine - EJS
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use('/', pages)
+app.use('/series', series)
+
+
+mongoose
+    .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.listen(port, () => console.log('Listening on port'+port))
+    })
+    .catch( e => {
+        console.log(e)
+    })
